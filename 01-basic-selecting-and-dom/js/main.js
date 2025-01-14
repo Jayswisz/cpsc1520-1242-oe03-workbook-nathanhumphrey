b9d78cd14f9feb3ccb136f225b7bd21167f92844
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-13 21:44:06 -0700

    Week 01 JS Basics Complete

--- 01-basic-selecting-and-dom/js/main.js
+++ 01-basic-selecting-and-dom/js/main.js
@@ -1,3 +1,16 @@
-let updateText = document.querySelector('#update-text > p');
+console.log('output to the browser console, useful for debugging');
 
+let updateText = document.querySelector('#update-text > p');
 updateText.innerHTML = 'Correct the typos in this text.';
+
+let updateHtml = document.querySelector('#update-html > p');
+let currentText = updateHtml.innerHTML;
+updateHtml.innerHTML = '<strong>' + currentText + '</strong>';
+
+let updateColour = document.querySelector('#update-colour > p');
+updateColour.style.color = 'blue';
+
+console.log('Update Colour P', updateColour);
+
+// on your own, try to wrap the corrected text in the first paragraph in <i> tags
+// on your own, change the colour of the second h2 to red
