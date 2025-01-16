5078fdefe63a18579a45a7ef0b0fe7e23af419d9
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-15 20:57:08 -0700

    Week 02 Functions and math payment

--- 02-function-math-basics/js/main.js
+++ 02-function-math-basics/js/main.js
@@ -1,2 +1,13 @@
 // Prompt for required inputs and display a monthly payment
 // Required input variables
+
+let principal = 1000;
+let interestRate = 0.05 / 12;
+let numberOfMonths = 5 * 12;
+let payment;
+
+let upper = interestRate * Math.pow(1 + interestRate, numberOfMonths);
+let bottom = Math.pow(1 + interestRate, numberOfMonths) - 1;
+payment = principal * (upper / bottom);
+
+console.log(payment);
