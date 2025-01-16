6001b3efa779777ba9d866c2a7bfe9c2190e2ac9
Nathan Humphrey <nathpaulhump@gmail.com>
2025-01-15 21:44:35 -0700

    Week 02 Functions and Math update comments

--- 02-function-math-basics/js/main.js
+++ 02-function-math-basics/js/main.js
@@ -1,22 +1,26 @@
 // Prompt for required inputs and display a monthly payment
-// Required input variables
 
+// Required input variables
 let principal;
 let interestRate;
 let numberOfMonths;
 let payment;
 
 // Capture input from the user
-// TODO: implement some type checking (if-else) on all inputs
+// TODO: implement some type checking (if-else, loops, etc.) on all inputs
+// Make use of isNaN to check for non-convertible values
 principal = parseFloat(prompt('Enter principal amount'));
-// Other options for converting
+// Other options for converting to a number
 // principal = Number(prompt('Enter principal amount'));
 // principal = +prompt('Enter principal amount');
 
 interestRate = parseFloat(prompt('Enter annual interest rate')) / 1200;
 numberOfMonths = parseInt(prompt('Enter number of years')) * 12;
 
-// If using a function expression, you must assign before use/calling
+// If using a function expression, you must assign before use/calling.
+// The calculatePayment function would need to be defined here, before
+// it is called below to be valid.
+//
 // const calculatePayment = function (p, i, n) {
 //   let upper = i * Math.pow(1 + i, n);
 //   let bottom = Math.pow(1 + i, n) - 1;
@@ -29,17 +33,24 @@ console.log(payment);
 
 displayPayment(payment);
 
-// If using declared functions, you can place them anywhere
-// Using JSDoc to document a function: https://jsdoc.app/
+// If using declared functions, you can place them anywhere; here
+// we place the calculatePayment and displayPayment functions at
+// the end of the file.
+//
+// We are also using JSDoc to document the functions: https://jsdoc.app/
 
 /**
  * Calculate the monthly payment for a loan.
+ * You can use the following site (or any other site) to check your results:
+ * https://www.calculator.net/payment-calculator.html
+ *
  * @param {number} p principal amount
  * @param {number} i monthly interest rate
  * @param {number} n number of months
  * @returns {number} monthly payment
  */
 function calculatePayment(p, i, n) {
+  // Monthly payment formula: https://en.wikipedia.org/wiki/Mortgage_calculator#Monthly_payment_formula
   let upper = i * Math.pow(1 + i, n);
   let bottom = Math.pow(1 + i, n) - 1;
   return p * (upper / bottom);
